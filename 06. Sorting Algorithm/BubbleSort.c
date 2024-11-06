0a1e4b02c8d150bfb8d85ed3df929fbaf555230b
dlgidrl2007 <165915519+dlgidrl2007@users.noreply.github.com>
2024-11-06 12:11:21 +0900

    Add files via upload

---
+++ 06. Sorting Algorithm/BubbleSort.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+// 버블정렬 : 다음 것과 비교하며 대소관계에 따라 자리 바꿈
+void bubbleSort(int a[], int n) {
+    int temp;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n-(i+1); j++) {
+            if (a[j] > a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {5, 15, 7, 20, 1 ,
+                 4, 10, 2, 8, 11,
+                 9, 6, 13, 18, 3,
+                 16, 12, 14, 17, 19};
+
+    bubbleSort(arr, 20);
+
+    for (int i = 0; i < 20; i++)
+        printf("%d ", arr[i]);
+}
